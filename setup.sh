eee5224f70b071e61d5f9b3c982ef1e528a6d665
Jon Blanton <jon@subless.com>
2023-02-23 19:03:04 -0500

    added ssh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ sudo mkdir /home/jon/airsonic
 sudo chmod a+rwx /home/jon/Dropbox
 sudo chmod a+rwx /home/jon/DropboxConfig
 sudo chmod a+rwx /home/jon/airsonic
+sudo dnf install openssh-server
+sudo systemctl enable sshd
+sudo systemctl start sshd
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
